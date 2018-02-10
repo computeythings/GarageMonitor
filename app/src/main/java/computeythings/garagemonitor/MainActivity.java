9c0de1788f31d0afb4655ee868e16760d4db7c79
computeythings <25091077+computeythings@users.noreply.github.com>
2018-02-09 23:30:30 -0800

    Added settings navigation drawer.

--- app/src/main/java/computeythings/garagemonitor/MainActivity.java
+++ app/src/main/java/computeythings/garagemonitor/MainActivity.java
@@ -9,15 +9,17 @@ import android.content.ServiceConnection;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -28,14 +30,17 @@ import org.json.JSONObject;
 
 import computeythings.garagemonitor.TCPSocketService.SocketServiceBinder;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MAIN_ACTIVITY";
-    private SwipeRefreshLayout mRefreshLayout;
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
     private BroadcastReceiver mDataReceiver;
     private TCPSocketService mSocketConnection;
+    private DrawerLayout mDrawer;
     private boolean mSocketBound;
+    private String mServerName;
+    private String mAPIKey;
+    private int mCertId;
+    private int mServerPort = 4444;
 
     private ServiceConnection mConnection;
 
@@ -43,19 +48,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Toolbar setup
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mRefreshLayout = findViewById(R.id.swipe_refresh);
-        mRefreshLayout.setOnRefreshListener(
+        //Navigation Drawer setup
+        mDrawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //Setup swipe to refresh
+        final SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setOnRefreshListener(
                 new SwipeRefreshLayout.OnRefreshListener() {
                     @Override
                     public void onRefresh() {
-                        new AsyncSocketRefresh(mRefreshLayout).executeOnExecutor(
+                        new AsyncSocketRefresh(swipeRefreshLayout).executeOnExecutor(
                                 AsyncTask.THREAD_POOL_EXECUTOR, mSocketConnection);
                     }
                 }
         );
 
+        buttonSetup();
+        mDataReceiver = new TCPBroadcastReceiver();
+    }
+
+    private void buttonSetup() {
         // Initialize Buttons
         FloatingActionButton refreshButton = findViewById(R.id.refresh_fab);
         refreshOnClick(refreshButton);
@@ -63,20 +85,17 @@ public class MainActivity extends AppCompatActivity {
         writeMessageOnClick(openButton, TCPSocketService.GARAGE_OPEN);
         FloatingActionButton closeButton = findViewById(R.id.close_fab);
         writeMessageOnClick(closeButton, TCPSocketService.GARAGE_CLOSE);
-
-        mDataReceiver = new TCPBroadcastReceiver();
     }
 
-    //TODO: pull server settings from settings file
     /*
         Create the intent to start the server from the selected server in the settings
      */
     private Intent getServerFromSettings() {
         Intent intent = new Intent(this, TCPSocketService.class);
-        intent.putExtra(TCPSocketService.SERVER_NAME, "");
-        intent.putExtra(TCPSocketService.API_KEY, "");
-        intent.putExtra(TCPSocketService.PORT_NUMBER, 4444);
-        intent.putExtra(TCPSocketService.CERT_ID, R.raw.sslcrt);
+        intent.putExtra(TCPSocketService.SERVER_NAME, mServerName);
+        intent.putExtra(TCPSocketService.API_KEY, mAPIKey);
+        intent.putExtra(TCPSocketService.PORT_NUMBER, mServerPort);
+        intent.putExtra(TCPSocketService.CERT_ID, mCertId);
         return intent;
     }
 
@@ -99,9 +118,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipe_refresh);
                 if (mSocketBound) {
-                    mRefreshLayout.setRefreshing(true);
-                    new AsyncSocketRefresh(mRefreshLayout).executeOnExecutor(
+                    swipeRefreshLayout.setRefreshing(true);
+                    new AsyncSocketRefresh(swipeRefreshLayout).executeOnExecutor(
                             AsyncTask.THREAD_POOL_EXECUTOR, mSocketConnection);
                 } else {
                     Toast.makeText(MainActivity.this, "Server disconnected!",
@@ -114,6 +134,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        //TODO: pull server settings from settings file
+        mServerName = "";
+        mAPIKey = "";
+        mCertId = R.raw.sslcrt;
+        mServerPort = 4444;
 
         mConnection = new TCPServiceConnection();
         // Create and bind a socket service based on currently selected server
@@ -133,25 +158,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
+    //TODO: Handle add server request
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        boolean serverSelected = false;
+        switch(item.getItemId()) {
+            case R.id.add_server_option:
+                Toast.makeText(this, "Oops I didn't implement this yet.",
+                        Toast.LENGTH_SHORT).show();
+                serverSelected = false;
+                break;
+            case R.id.no_servers:
+                return false; // Don't need to close the drawer if they tap this
         }
 
-        return super.onOptionsItemSelected(item);
+        mDrawer.closeDrawer(GravityCompat.START);
+        return serverSelected;
     }
 
     /*
